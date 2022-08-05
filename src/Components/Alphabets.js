e807d821f8362cfed466cce2e5153b2ccbc238df
ayushjii <pawangoyal6789@gmail.com>
2022-08-05 11:54:58 +0530

    f

--- src/Components/Alphabets.js
+++ src/Components/Alphabets.js
@@ -4,7 +4,7 @@ const useAlphabets = () => {
     const [alphabets, setAlphabets] = useState([]);
     useEffect(() => {
         const getAlphabets = async () => {
-             fetch("http://localhost:5000/")
+             fetch("https://floating-sands-78044.herokuapp.com/alphabets")
                 .then((response) => response.json())
                 .then((data) => {
                     setAlphabets(data);
